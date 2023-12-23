66ac3be11f7313382b6878dd2fc318f50e02eac6
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-12-23 18:59:14 -0300

    Inclusao de rotas

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import { useEffect, useState } from "react";
-import InitialPage from "./components/initialPage";
-import SecondaryPage from "./components/secondaryPage";
+import InitialPage from './components/initialPage';
+import SecondaryPage from './components/secondaryPage';
+import {Routes, Route, Link} from 'react-router-dom';
 
-const App = () => 
+export default function App() 
 {
-  const [page, setPage] = useState(0)
-
-  useEffect(
-    ()=>
-    {
-      const url = window.location.href;
-      const splitedUrl = url.split('?');
-      setPage(Number(splitedUrl[1]));
-    }, []
-  )
-
-  const openPage = (index)=>
-  {
-    window.open('http://localhost:3000?'+index, '_self');
-  }
-
-  const returnPage = ()=>
-  {
-    if(page === 1) return <InitialPage />
-    else if(page === 2) return <SecondaryPage />
-    return(
-      <>
-        <button onClick={()=>openPage(1)}>Página Inicial</button> <br />
-        <button onClick={()=>openPage(2)}>Página Secundaria </button>
-      </>
-    )
-  }
-
-  return (
+  return(
     <>
-      {returnPage()}
+      <header>
+        <Link to='/initial-page'> InitialPage </Link>
+        <br />
+        <Link to='/secondary-page'> SecondaryPage </Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/initial-page' element={<InitialPage />} />
+          <Route path='/secondary-page' element={<SecondaryPage />} />
+        </Routes>
+      </main>
     </>
   )
 }
-
-export default App;
